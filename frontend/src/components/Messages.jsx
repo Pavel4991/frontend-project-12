89419a1dd08c056abfd5d3b0282934f51925b310
Pavel Dzianisau <pdenisov327@gmail.com>
2026-05-26 12:07:22 +0300

    added some fixes

--- frontend/src/components/Messages.jsx
+++ frontend/src/components/Messages.jsx
@@ -25,10 +25,10 @@ const Messages = () => {
     if (messagesBoxRef.current) {
       messagesBoxRef.current.scrollTo({
         top: messagesBoxRef.current.scrollHeight,
-        behavior: 'auto'
+        behavior: 'auto',
       })
     }
-  }, [channelsMessages]) 
+  }, [channelsMessages])
 
   const handleSubmit = async (values) => {
     const newMessage = { body: values.body, channelId: activeChannel.id, username: username }
@@ -47,19 +47,19 @@ const Messages = () => {
           </p>
           <span className="text-muted">{t('ui.homePage.message', { count: channelsMessages.length })}</span>
         </div>
-        <div id="messages-box" className="chat-messages overflow-auto px-5 " ref={messagesBoxRef} >
+        <div id="messages-box" className="chat-messages overflow-auto px-5 " ref={messagesBoxRef}>
           {channelsMessages.map(message => (<Message key={message.id} username={message.username} body={message.body} />))}
         </div>
         <div className="mt-auto px-5 py-3">
           <Formik
             initialValues={{ body: '' }}
-            onSubmit={async (values, { setSubmitting, resetForm}) => {
+            onSubmit={async (values, { setSubmitting, resetForm }) => {
               handleSubmit(values)
               resetForm()
               setSubmitting(false)
             }}
           >
-            {() => (
+            {({ values, isSubmitting }) => (
               <Form className="py-1 border rounded-2" noValidate="">
                 <div className="input-group has-validation">
                   <Field
@@ -68,7 +68,11 @@ const Messages = () => {
                     className="border-0 p-0 ps-2 form-control"
                     placeholder="Введите сообщение..."
                   />
-                  <button type="submit" disabled="" className="btn btn-group-vertical">
+                  <button
+                    type="submit" disabled=""
+                    className="btn btn-group-vertical"
+                    disabled={!values.body.trim() || isSubmitting}
+                  >
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="20" height="20" fill="currentColor" className="bi bi-arrow-right-square">
                       <path fillRule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm4.5 5.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z">
                       </path>
